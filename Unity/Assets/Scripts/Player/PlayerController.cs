d7eabaeee29363e2852bd2e78efcc16877c6d26b
Thomas Faurbye Nielsen <Archanium@gmail.com>
2015-02-14 19:26:18 +0100

    Added a simple script

---
+++ Unity/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float maxHorizontalSpeed = 30f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float move = Input.GetAxis ("Horizontal");
+		print ("Move: " + move.ToString());
+		rigidbody2D.AddForce(new Vector2(move*this.maxHorizontalSpeed,rigidbody2D.velocity.y));
+	}
+
+}
